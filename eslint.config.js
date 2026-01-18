7c2467b473d5b43e13a3669bf496f2cc450a8ae6
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-18 10:09:53 +0000

    chore: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+
+    ignores: [
+      "node_modules/**", 
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      ".git/**",
+      "logs/**",
+      "tmp/**",
+      "*.min.js",
+    ],
+
+    plugins: { 
+      js 
+    }, 
+
+    extends: ["js/recommended"], 
+
+    languageOptions: { 
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node 
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+]);
