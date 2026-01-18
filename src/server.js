e2e2d6551bbafe91a9cfb4cf4f915fae0b1f39c4
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-18 16:59:26 +0000

    feat: connect database on bootstrap

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import http from 'http';
 
 import app from './app.js';
+import { connectToDB } from './config/db.js';
 import { env } from './config/env.js';
 
 const PORT = env.port || 3000;
 
-export const bootstrap = () => {
-  const server = http.createServer(app);
+export const bootstrap = async () => {
+  try {
+    // Connect DB
+    await connectToDB();
 
-  server.listen(PORT, () => {
-    console.log(`Server is running on http:localhost:${PORT}`);
-  });
+    // Start the HTTP server
+    const server = http.createServer(app);
+
+    server.listen(PORT, () => {
+      console.log(`Server is running on http:localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to bootstrap server:', error);
+    process.exit(1);
+  }
 };
