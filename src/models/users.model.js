e5c0fe9b77d2daf37be93c83e99f0948df3cec9a
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-30 18:16:58 +0000

    feat: create user schema & model

---
+++ src/models/users.model.js
@@ -0,0 +1,71 @@
+import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  try {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.isPasswordMatch = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+/**
+ * Hide password in JSON response
+ */
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
