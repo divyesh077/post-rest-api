c346babb8712fd70e2ccf41a33062649fcf8729a
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-31 06:21:37 +0000

    set jwt and smtp env config

--- src/config/env.js
+++ src/config/env.js
@@ -16,4 +16,27 @@ export const env = {
   mongodb: {
     uri: getEnv('MONGODB_URI'),
   },
+  jwt: {
+    accessSecret: getEnv('JWT_ACCESS_SECRET'),
+    accessExpiresIn: getEnv('JWT_ACCESS_EXPIRES_IN'),
+
+    refreshSecret: getEnv('JWT_REFRESH_SECRET'),
+    refreshExpiresIn: getEnv('JWT_REFRESH_EXPIRES_IN'),
+
+    emailVerifySecret: getEnv('EMAIL_VERIFY_SECRET'),
+    emailVerifyExpiresIn: getEnv('EMAIL_VERIFY_EXPIRES_IN'),
+
+    passwordResetSecret: getEnv('PASSWORD_RESET_SECRET'),
+    passwordResetExpiresIn: getEnv('PASSWORD_RESET_EXPIRES_IN'),
+  },
+  smtp: {
+    host: getEnv('SMTP_HOST'),
+    port: Number(getEnv('SMTP_PORT', 587)),
+    security: Number(getEnv('SMTP_PORT', 587)) === 465,
+    auth: {
+      user: getEnv('SMTP_USER'),
+      pass: getEnv('SMTP_PASS'),
+    },
+    from: getEnv('SMTP_FROM'),
+  },
 };
