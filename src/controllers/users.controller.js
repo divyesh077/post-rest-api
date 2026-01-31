c8c4a0574772cdf393533f2fce40b234ea71420a
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-31 08:02:43 +0000

    feat: add getUserById api

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -12,3 +12,19 @@ export const getUsers = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await usersService.getUserById(userId);
+
+    res.status(200).json({
+      success: true,
+      message: 'users fetch successfully',
+      user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getUsers } from '../../controllers/users.controller.js';
+import { getUserById, getUsers } from '../../controllers/users.controller.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:userId', getUserById);
 
 export default router;
