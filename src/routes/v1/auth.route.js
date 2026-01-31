131859bf2268ad7112eaaf5499bd7bb2ad847648
Divyesh <rathvadivyesh4317@gmail.com>
2026-01-31 12:52:30 +0000

    feat: add auth validators for apis

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -12,8 +12,18 @@ import {
   verifyEmail,
 } from '../../controllers/auth.controller.js';
 import { auth } from '../../middlewares/auth.middleware.js';
-import { validateBody } from '../../middlewares/validate.middleware.js';
-import { loginUserSchema, registerUserSchema } from '../../validators/auth.validator.js';
+import { validateBody, validateQuery } from '../../middlewares/validate.middleware.js';
+import {
+  forgotPasswordSchema,
+  loginUserSchema,
+  logoutUserSchema,
+  refreshTokenSchema,
+  registerUserSchema,
+  resetPasswordBodySchema,
+  resetPasswordQuerySchema,
+  sendVerificationEmailSchema,
+  verifyEmailQuerySchema,
+} from '../../validators/auth.validator.js';
 
 const router = express.Router();
 
@@ -21,18 +31,27 @@ router.post('/signup', validateBody(registerUserSchema), signup);
 
 router.post('/signin', validateBody(loginUserSchema), signin);
 
-router.post('/signout', auth, signout);
+router.post('/signout', validateBody(logoutUserSchema), auth, signout);
 
 router.post('/signout-all-devices', auth, signoutAllDevicesForUser);
 
-router.post('/refresh-token', refresh);
+router.post('/refresh-token', validateBody(refreshTokenSchema), refresh);
 
-router.post('/send-verification-email', sendVerificationEmail);
+router.post(
+  '/send-verification-email',
+  validateBody(sendVerificationEmailSchema),
+  sendVerificationEmail
+);
 
-router.post('/verify-email', verifyEmail);
+router.post('/verify-email', validateQuery(verifyEmailQuerySchema), verifyEmail);
 
-router.post('/forgot-password', forgotPassword);
+router.post('/forgot-password', validateBody(forgotPasswordSchema), forgotPassword);
 
-router.post('/reset-password', resetPassword);
+router.post(
+  '/reset-password',
+  validateQuery(resetPasswordQuerySchema),
+  validateBody(resetPasswordBodySchema),
+  resetPassword
+);
 
 export default router;
--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -1,3 +1,5 @@
+import z from 'zod';
+
 import { userSchema } from './users.validator.js';
 
 export const registerUserSchema = userSchema.pick({
@@ -9,4 +11,32 @@ export const registerUserSchema = userSchema.pick({
 export const loginUserSchema = registerUserSchema.pick({
   email: true,
   password: true,
+});
+
+export const logoutUserSchema = z.object({
+  refreshToken: z.string().nonempty(),
+});
+
+export const refreshTokenSchema = z.object({
+  refreshToken: z.string().nonempty(),
+});
+
+export const sendVerificationEmailSchema = registerUserSchema.pick({
+  email: true,
+});
+
+export const verifyEmailQuerySchema = z.object({
+  token: z.string().nonempty(),
+});
+
+export const forgotPasswordSchema = registerUserSchema.pick({
+  email: true,
+});
+
+export const resetPasswordBodySchema = registerUserSchema.pick({
+  password: true,
+});
+
+export const resetPasswordQuerySchema = z.object({
+  token: z.string().nonempty(),
 });
